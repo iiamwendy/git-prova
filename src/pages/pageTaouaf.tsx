4b45358408f5f25ce13ab9df72dd0f4694144c20
iiamwendy <widadtaouaf03@gmail.com>
2023-11-06 16:02:20 +0100

    aggiunta di <p>

--- src/pages/pageTaouaf.tsx
+++ src/pages/pageTaouaf.tsx
@@ -5,7 +5,10 @@ function pageTaouaf() {
   return (
     <div>
       <Logo />
-      <p>Taouaf Widad</p>
+      <h1>Taouaf Widad</h1>
+      <p>Soluzione breve:ho creato una nuova brench che è la copia della master,così le modifiche rimangono salvate ,mentre nella master 
+        per eliminare le modifiche fatte ho inserito questi componenti : git checkout master / git log / git reset --hard (codice della commit)
+      </p>
     </div>
   );
 }
