cc260c7d5949ae3c095ecdd0f79bd6b4a4db0327
iiamwendy <widadtaouaf03@gmail.com>
2023-11-06 16:16:38 +0100

    Aggiunta soluzione per portare avanti la branch dedicata

--- src/pages/pageTaouaf.tsx
+++ src/pages/pageTaouaf.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+
 
 function pageTaouaf() {
   return (
     <div>
       <h1>Taouaf Widad</h1>
+      <p>Soluzione per ripostarla avanti la branch: sulla branch dedicata devi inserire il comando     
+        git checkout feature/taouaf-init
+        devo aggioranre la branch con le ultime modifiche di master :git pull origin master
+        ora devo eseguire una commit delle modifiche sulla mia branch : git add .    
+        git commit -m " aggiornamento"
+        ora faccio la push delle modifiche :
+        git push origin feature/taouaf-init
+</p>
     </div>
   );
 }
